1e19f3beecad6e23d3d1773c0e8aa4adba5a20c7
Aumlan Abrar <44971908+aumlan@users.noreply.github.com>
2021-02-14 16:54:58 +0600

    AppendChild

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-// Get Element by ClassName
-/*const item = document.getElementsByClassName('collection-item');
-console.log(item);
+const li = document.createElement('li');
+li.className = 'collection-item';
+li.textContent = 'New Item';
+document.querySelector('ul.collection').appendChild(li);
 
-item[2].style.color = 'red';
+const link = document.createElement('a');
+link.className = 'delete-item secondary-content';
+link.innerHTML = '<i class="fa fa-remove">'
 
-const itemlist = document.querySelector('ul').getElementsByClassName('collection-item');
-
-console.log(itemlist);*/
-
-// Get Element by Tag
-// querySelectorAll()
-/*const items = document.querySelectorAll('ul li');
-console.log(items);
-
-items.forEach(item => {
-    item.style.color = 'red';
-    //item.textContent = 'hello';
-});*/
-
-//* Traverse
-//children
-let val;
-const list = document.querySelector('ul.collection');
-
-val = list.children[2];
-let val2 = list.children[2].children[0];
-
-//console.log(val2);
-
-
-//Parent
-let pp;
-const list_item = document.querySelector('li.collection-item');
-
-pp = list.parentElement;
-
-console.log(pp);
+li.appendChild(link);
+console.log(li);
